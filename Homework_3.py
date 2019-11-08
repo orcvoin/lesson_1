2b3c4c86209c48030070e1e052c6c38be5eb80ac
andy <orcvoin@gmail.com>
2019-11-08 10:35:56 +0500

    filled with code

--- Homework_3.py
+++ Homework_3.py
@@ -1,1 +1,12 @@
-#
+# sum
+print('Введите число: ')
+a = int(input())
+# Способ 1
+number = a + (a*10 + a) + (a*100 + a*10 + a)
+# Способ 2
+b = a*10 + a
+c = a*100 + a*10 + a
+number_2 = a + b +c
+# принт
+print(number)
+print(number_2)
